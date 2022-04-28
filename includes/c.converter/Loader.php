95709aa1275f9e842adbda2a290a7ef1c13e94a9
Michael Uno <michael2@michaeluno.jp>
2022-04-29 07:49:22 +0900

    Style: Clean code
    Remove unused code.

--- includes/c.converter/Loader.php
+++ includes/c.converter/Loader.php
@@ -27,12 +27,6 @@ class Loader extends LoaderAbstract {
     ];
 
     /**
-     * @since 0.1.0
-     * @var   string The post type slug.
-     */
-    static public $sPostTypeSlug = 'unit_to_wc_products';
-
-    /**
      * Loads the component members.
      * @since 0.1.0
      */
@@ -49,21 +43,6 @@ class Loader extends LoaderAbstract {
         );
         $this->aMembers[ get_class( $_oMetaBoxMain ) ] = $_oMetaBoxMain;
 
-        // @deprecated 0.1.0
-        // $_oPostType = new PostType(
-        //     self::$sPostTypeSlug,  // slug
-        //     null,        // post type argument. This is defined in the class.
-        //     \AutoAmazonLinks\WooCommerceProducts\App::$sFilePath // script path
-        // );
-        // $this->aMembers[ get_class( $_oPostType ) ] = $_oPostType;
-        // $_oMetaBoxUnitSelector = new UnitSelector(
-        //     null, // meta box ID - null to auto-generate
-        //     __( 'Units', 'amazon-auto-links' ),
-        //     [ self::$sPostTypeSlug ],
-        //     'normal', // context (what kind of meta-box this is)
-        //     'default' // priority
-        // );
-        // $this->aMembers[ get_class( $_oMetaBoxUnitSelector ) ] = $_oMetaBoxUnitSelector;
     }
 
 }
