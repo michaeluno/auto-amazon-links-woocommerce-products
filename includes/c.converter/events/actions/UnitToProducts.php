7f983a8f5912d6d312bdd3db24deba56804e6c6d
Michael Uno <michael2@michaeluno.jp>
2022-04-30 07:31:10 +0900

    Refactor: Move the part converting AAL product to WC product to a utility class

--- includes/c.converter/events/actions/UnitToProducts.php
+++ includes/c.converter/events/actions/UnitToProducts.php
@@ -57,6 +57,8 @@ class UnitToProducts implements MemberInterface {
                 $this->___tryConvertUnitProductToWooCommerceProduct( $_aProduct );
             } catch ( \Exception $_oException ) {
 
+                // @todo Log error
+                // new \AmazonAutoLinks_Error()
                 // \AmazonAutoLinks_Debug::log( 'Unit ID: ' . $iUnitID . ': '. $_oException->getCode() . ' :' . $_oException->getMessage() );
     
             }
@@ -97,98 +99,13 @@ class UnitToProducts implements MemberInterface {
             throw new \Exception( $aItem[ 'ASIN' ] . ' The product has not been updated yet. Last updated: ' . $_iLastUpdatedTime . ' This updated: ' . $_iThisUpdatedTime );
         }
 
-        $_oWCProduct       = new \WC_Product_Simple( $_iProductID );
-        $_oWCProduct->set_name( $aItem[ 'title' ] );
-        $_oWCProduct->set_status( 'publish' );
-        $_oWCProduct->set_catalog_visibility( 'visible' );
-        $_oWCProduct->set_sold_individually( false );   // allow multiple quantities
-        $_oWCProduct->set_downloadable( false );
-        $_oWCProduct->set_virtual( true );
-
-        // Product meta
-        $_oWCProduct->set_sku( $_sSKU );
-        $_oWCProduct->set_short_description( $this->oUtil->getElement( $aItem, [ 'text_description' ] ) );
-        $_oWCProduct->set_description( $this->oUtil->getElement( $aItem, [ 'feature' ] ) );
-        $_sPriceProper     = $this->getPriceAmountExtracted(
-            $this->oUtil->getElement(
-                $aItem,
-                [ 'price_amount' ], // for Product Search and PA-API units
-                $this->oUtil->getElement( $aItem, [ 'formatted_price' ], '' )   // for Category units
-            )
-        );
-        $_sPriceDiscounted = $this->getPriceAmountExtracted(
-            $this->oUtil->getElement(
-                $aItem, [ 'discounted_price' ], // for PA-API units
-                $this->oUtil->getElement( $aItem, [ 'discounted_price_amount' ], '' )    // for Product Search units @todo set a proper element
-            )
-        );
-        $_sPriceDisplay    = strlen( $_sPriceDiscounted ) ? $_sPriceDiscounted : $_sPriceProper;
-        if ( strlen( $_sPriceDiscounted ) ) {
-            $_oWCProduct->set_sale_price( $_sPriceDisplay );
-        }
-        if ( $_sPriceDisplay ) {
-            $_oWCProduct->set_price( $_sPriceDisplay );
-        }
-        if ( strlen( $_sPriceProper ) ) {
-            $_oWCProduct->set_regular_price( $_sPriceProper );
-        }
-        $_dRating = ( ( integer ) $this->oUtil->getElement( $aItem, [ 'rating' ] ) ) / 10;
-        if ( $_dRating ) {
-            $_oWCProduct->set_average_rating( $_dRating );
-        }
-        $_iNumberOfRatings = ( integer ) $this->oUtil->getElement( $aItem, [ 'number_of_reviews' ] );
-        if ( $_iNumberOfRatings ) {
-            $_oWCProduct->set_review_count( $_iNumberOfRatings );
-        }
-
-        $_iID = $_oWCProduct->save();
-        if ( ! $_iID ) {
+        $_iProductID       = $this->getWooCommerceProductCreatedFromUnitProduct( $aItem, $_iProductID );
+        if ( ! $_iProductID ) {
             throw new \Exception( $aItem[ 'ASIN' ] . ' Failed to create/update a product.' );
         }
-
-        // Plugin specific post meta
-        update_post_meta( $_iID, '_product_url', $aItem[ 'product_url' ] );
-        update_post_meta( $_iID, '_thumbnail_url', $aItem[ 'thumbnail_url' ] );
-        update_post_meta( $_iID, '_updated_time', $_iThisUpdatedTime );
-        update_post_meta( $_iID, '_asin', $aItem[ 'ASIN' ] );
-
-        $this->___setCategoriesFromItem( $_iID, $aItem );
-        
+        update_post_meta( $_iProductID, '_updated_time', $_iThisUpdatedTime );
         $this->iLatestUpdated = $_iThisUpdatedTime > $this->iLatestUpdated ? $_iThisUpdatedTime : $this->iLatestUpdated;
 
     }
-        private function ___setCategoriesFromItem( $iPostID, $aItem ) {
-            foreach( array_values( $this->oUtil->getElementAsArray( $aItem, [ '_categories' ] ) ) as $_aCategories ) {
-                $this->___setCategoriesWithHierarchicalTerms( $iPostID, $_aCategories );
-            }
-        }
-            private function ___setCategoriesWithHierarchicalTerms( $iPostID, array $aTerms ) {
-                $_aTermIDs    = [];
-                foreach( $aTerms as $_iDepth => $_sTerm ) {
-                    $_aTermIDs[ $_iDepth ] = $this->___createTermByDepth(
-                        $_sTerm,
-                        $_iDepth,
-                        $this->oUtil->getElement( $_aTermIDs, [ $_iDepth - 1 ], 0 )
-                    );
-                }
-                if ( empty( $_aTermIDs ) ) {
-                    return;
-                }
-                wp_set_object_terms( $iPostID, $_aTermIDs, 'product_cat' );
-            }
-                private function ___createTermByDepth( $sTerm, $iDepth, $iParentTermID ) {
-                    $_aoResult = wp_insert_term(
-                      $sTerm, // the term
-                      'product_cat', // the WooCommerce category taxonomy
-                      array(
-                          'slug'   => sanitize_title( $sTerm ) . ( $iDepth ? '-' . $iDepth : '' ),  // slugs shouldn't be auto-generated by WP Core because the same term labels can occur. So we distinguish terms with hierarchical depths.
-                          'parent' => $iParentTermID,
-                      )
-                    );
-                    if ( is_wp_error( $_aoResult ) && $_aoResult->get_error_code() === 'term_exists' ) {
-                        return ( integer ) $_aoResult->get_error_data( 'term_exists' );
-                    }
-                    return ( integer ) $this->oUtil->getElement( $_aoResult, [ 'term_id' ] );
-                }
 
 }
--- includes/commons/utilities/Utility.php
+++ includes/commons/utilities/Utility.php
@@ -7,6 +7,115 @@ namespace AutoAmazonLinks\WooCommerceProducts\Commons;
 trait Utility {
 
     /**
+     * @since  0.3.0
+     * @remark This does not store the `_updated_time` meta value as there are use cases which should avoid it.
+     * @param  array   $aItem   A unit product.
+     * @param  integer $iWCProductID    An existing WooCommerce product ID. If this is given, the existing product will be updated. Otherwise, a new product will be created.
+     * @return integer The created WooCommerce product ID.
+     */
+    static public function getWooCommerceProductCreatedFromUnitProduct( array $aItem, $iWCProductID=0 ) {
+        
+        $_oUtil           = new \AmazonAutoLinks_Utility();
+        $_sSKU            = $_oUtil->getElement( $aItem, [ 'product_id' ], $_oUtil->getElement( $aItem, [ 'ASIN' ] ) );
+        
+        if ( ! $iWCProductID ) {
+            $iWCProductID = wc_get_product_id_by_sku( $_sSKU );            
+        }
+        
+        $_oWCProduct      = new \WC_Product_Simple( $iWCProductID );
+        $_oWCProduct->set_name( $aItem[ 'title' ] );
+        $_oWCProduct->set_status( 'publish' );
+        $_oWCProduct->set_catalog_visibility( 'visible' );
+        $_oWCProduct->set_sold_individually( false );   // allow multiple quantities
+        $_oWCProduct->set_downloadable( false );
+        $_oWCProduct->set_virtual( true );
+
+        // Product meta
+        $_oWCProduct->set_sku( $_sSKU );
+        $_oWCProduct->set_short_description( $_oUtil->getElement( $aItem, [ 'text_description' ] ) );
+        $_oWCProduct->set_description( $_oUtil->getElement( $aItem, [ 'feature' ] ) );
+        $_sPriceProper     = self::getPriceAmountExtracted(
+            $_oUtil->getElement(
+                $aItem,
+                [ 'price_amount' ], // for Product Search and PA-API units
+                $_oUtil->getElement( $aItem, [ 'formatted_price' ], '' )   // for Category units
+            )
+        );
+        $_sPriceDiscounted = self::getPriceAmountExtracted(
+            $_oUtil->getElement(
+                $aItem, [ 'discounted_price' ], // for PA-API units
+                $_oUtil->getElement( $aItem, [ 'discounted_price_amount' ], '' )    // for Product Search units @todo set a proper element
+            )
+        );
+        $_sPriceDisplay    = strlen( $_sPriceDiscounted ) ? $_sPriceDiscounted : $_sPriceProper;
+        if ( strlen( $_sPriceDiscounted ) ) {
+            $_oWCProduct->set_sale_price( $_sPriceDisplay );
+        }
+        if ( $_sPriceDisplay ) {
+            $_oWCProduct->set_price( $_sPriceDisplay );
+        }
+        if ( strlen( $_sPriceProper ) ) {
+            $_oWCProduct->set_regular_price( $_sPriceProper );
+        }
+        $_dRating = ( ( integer ) $_oUtil->getElement( $aItem, [ 'rating' ] ) ) / 10;
+        if ( $_dRating ) {
+            $_oWCProduct->set_average_rating( $_dRating );
+        }
+        $_iNumberOfRatings = ( integer ) $_oUtil->getElement( $aItem, [ 'number_of_reviews' ] );
+        if ( $_iNumberOfRatings ) {
+            $_oWCProduct->set_review_count( $_iNumberOfRatings );
+        }
+
+        $_iID = $_oWCProduct->save();
+        if ( ! $_iID ) {
+            return 0;
+        }
+
+        // Plugin specific post meta
+        update_post_meta( $_iID, '_product_url', $aItem[ 'product_url' ] );
+        update_post_meta( $_iID, '_thumbnail_url', $aItem[ 'thumbnail_url' ] );
+        update_post_meta( $_iID, '_asin', $aItem[ 'ASIN' ] );
+
+        self::___setCategoriesFromItem( $_iID, $aItem );
+        return $_iID;
+        
+    }
+
+        static private function ___setCategoriesFromItem( $iPostID, $aItem ) {
+            foreach( array_values( \AmazonAutoLinks_Utility::getElementAsArray( $aItem, [ '_categories' ] ) ) as $_aCategories ) {
+                self::___setCategoriesWithHierarchicalTerms( $iPostID, $_aCategories );
+            }
+        }
+            static private function ___setCategoriesWithHierarchicalTerms( $iPostID, array $aTerms ) {
+                $_aTermIDs = [];
+                foreach( $aTerms as $_iDepth => $_sTerm ) {
+                    $_aTermIDs[ $_iDepth ] = self::___createTermByDepth(
+                        $_sTerm,
+                        $_iDepth,
+                        \AmazonAutoLinks_Utility::getElement( $_aTermIDs, [ $_iDepth - 1 ], 0 )
+                    );
+                }
+                if ( empty( $_aTermIDs ) ) {
+                    return;
+                }
+                wp_set_object_terms( $iPostID, $_aTermIDs, 'product_cat' );
+            }
+                static private function ___createTermByDepth( $sTerm, $iDepth, $iParentTermID ) {
+                    $_aoResult = wp_insert_term(
+                      $sTerm, // the term
+                      'product_cat', // the WooCommerce category taxonomy
+                      array(
+                          'slug'   => sanitize_title( $sTerm ) . ( $iDepth ? '-' . $iDepth : '' ),  // slugs shouldn't be auto-generated by WP Core because the same term labels can occur. So we distinguish terms with hierarchical depths.
+                          'parent' => $iParentTermID,
+                      )
+                    );
+                    if ( is_wp_error( $_aoResult ) && $_aoResult->get_error_code() === 'term_exists' ) {
+                        return ( integer ) $_aoResult->get_error_data( 'term_exists' );
+                    }
+                    return ( integer ) \AmazonAutoLinks_Utility::getElement( $_aoResult, [ 'term_id' ] );
+                }    
+    
+    /**
      * Extracts the first-found comma-separated digits from a string.
      * Possible Cases:
      *  - discount + proper: <span class="amazon-prices"><span class="proper-price"><s>$3,610 ($361 / item)</s></span> <span class="offered-price">$2,796 ($280 / item)</span></span>
