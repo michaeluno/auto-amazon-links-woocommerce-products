0297d2d6b7c5a133c73291fc55f568394a2d5cb2
Michael Uno <michael2@michaeluno.jp>
2022-05-02 10:45:02 +0900

    Feature: Add the ability to automatically convert products when the user newly enables the `Unit to WooCommerce Product Converter` option

--- includes/c.converter/admin/post-meta-boxes/Main.php
+++ includes/c.converter/admin/post-meta-boxes/Main.php
@@ -17,4 +17,28 @@ class Main extends PostMetaBoxAbstract {
         '\\AutoAmazonLinks\\WooCommerceProducts\\Converter\\Admin\\FormFields\\Main',
     ];
 
+    /**
+     * @param  array $aInputs
+     * @param  array $aOldInputs
+     * @param  \AmazonAutoLinks_AdminPageFramework_MetaBox $oFactory
+     * @return array
+     */
+    public function validate( $aInputs, $aOldInputs, $oFactory ) {
+
+        if ( ! $this->oUtil->getElement( $aInputs, [ '_unit_to_wc_products_enable' ] ) ) {
+            return $aInputs;
+        }
+        // At this point, the option is enabled.
+
+        // If the option is previously enabled, no need to take action.
+        if ( $this->oUtil->getElement( $aOldInputs, [ '_unit_to_wc_products_enable' ] ) ) {
+            return $aInputs;
+        }
+
+        // At this point, the user newly enabled the option.
+        \AmazonAutoLinks_Event_Scheduler::prefetch( \AmazonAutoLinks_PluginUtility::getCurrentPostID() );
+        return $aInputs;
+
+    }
+
 }
